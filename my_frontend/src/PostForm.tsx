43d04dd60447611ec645d8560964b82731733e0e
Byron Katz <byronka@msn.com>
2021-09-21 12:27:11 -0400

    adding a success indicator

--- my_frontend/src/PostForm.tsx
+++ my_frontend/src/PostForm.tsx
@@ -3,6 +3,7 @@ import React, { SyntheticEvent, useState } from "react";
 const PostForm = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [success, setSuccess] = useState(false);
 
   const handleNameChange = (event: React.FormEvent<HTMLInputElement>) => {
     setName(event.currentTarget.value);
@@ -25,33 +26,36 @@ const PostForm = () => {
     fetch("http://localhost:3000/posts", requestOptions)
       .then((response) => response.json())
       .then(() => {
-        alert("Yay! success!!!");
-        return "hello";
+        setSuccess(true);
       });
   };
 
+  const successBox = success ? <div>Success</div> : <div></div>;
   return (
-    <form action="#" method="POST" onSubmit={submitData}>
-      <label htmlFor="nameInput">Name</label>
-      <input
-        id="nameInput"
-        name="nameInput"
-        type="text"
-        value={name}
-        onChange={handleNameChange}
-      />
+    <>
+      <form action="#" method="POST" onSubmit={submitData}>
+        <label htmlFor="nameInput">Name</label>
+        <input
+          id="nameInput"
+          name="nameInput"
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+        />
 
-      <label htmlFor="descriptionInput">Description</label>
-      <input
-        id="descriptionInput"
-        name="descriptionInput"
-        type="text"
-        value={description}
-        onChange={handleDescriptionChange}
-      />
+        <label htmlFor="descriptionInput">Description</label>
+        <input
+          id="descriptionInput"
+          name="descriptionInput"
+          type="text"
+          value={description}
+          onChange={handleDescriptionChange}
+        />
 
-      <button>Enter new post</button>
-    </form>
+        <button>Enter new post</button>
+      </form>
+      {successBox}
+    </>
   );
 };
 
