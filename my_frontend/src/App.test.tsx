d43fa2350effb2028b0d6b9468e616ef7ec3ef6e
Byron Katz <byronka@msn.com>
2021-09-21 12:27:37 -0400

    test is broken

--- my_frontend/src/App.test.tsx
+++ my_frontend/src/App.test.tsx
@@ -1,23 +1,25 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import userEvent from '@testing-library/user-event'
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("test", () => {
-  expect(true).toBeTruthy()
-})
+  expect(true).toBeTruthy();
+});
 
 test("happy path - I should be able to create a new Post", async () => {
-    const nameInput = screen.getByLabelText('name')
-    userEvent.type(nameInput, 'bob')
+  render(<App />);
 
-    const descInput = screen.getByLabelText('desc')
-    userEvent.type(descInput, 'Hi I am the description')
+  const nameInput = screen.getByLabelText("Name");
+  userEvent.type(nameInput, "bob");
 
-    const button = screen.getByRole('button')
-    userEvent.click(button)
+  const descInput = screen.getByLabelText("Description");
+  userEvent.type(descInput, "Hi I am the description");
 
-    const successAlert = await screen.findByRole('success box')
-    expect(successAlert).toBeVisible
-})
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const successAlert = await screen.findByText("Success");
+  expect(successAlert).toBeVisible;
+});
