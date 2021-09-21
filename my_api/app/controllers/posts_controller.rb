103b22e44c8f09567a91dc65dc2cc4968ac72337
Byron Katz <byronka@msn.com>
2021-09-21 12:10:08 -0400

    a new "create" endpoint on the server

--- my_api/app/controllers/posts_controller.rb
+++ my_api/app/controllers/posts_controller.rb
@@ -2,4 +2,8 @@ class PostsController < ApplicationController
   def index
     render(json: Post.select("id, name, description"))
   end
+
+  def create
+    render(json: {"success": true})
+  end
 end
