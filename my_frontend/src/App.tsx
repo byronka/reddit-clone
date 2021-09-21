0fcbdd33f29f2462c83f72602b59d18b6b20a031
Byron Katz <byronka@msn.com>
2021-09-21 10:14:01 -0400

    handling errors during fetch clear

--- my_frontend/src/App.tsx
+++ my_frontend/src/App.tsx
@@ -4,26 +4,41 @@ const API_URL = 'http://localhost:3000/posts'
 
 interface Post {
   name: string
+  description: string
 }
 
 const App = () => {
   let post: Post;
   let setPost: any;
-  [post, setPost] = useState({name: ""})
+  [post, setPost] = useState({name: "", description: ""})
 
   useEffect(() => {
     async function requestPost() {
-      const res = await fetch(API_URL);
-      const json = await res.json();
-      setPost(json)
-      return json;
+      try {
+        let res = await fetch(API_URL)
+        const json = await res.json();
+        setPost(json)
+        return json;
+      } catch(err) {
+        if (err instanceof TypeError) {
+          if(err.message === 'Failed to fetch') {
+            alert('failed to fetch - is the server down?')
+          }
+        } else {
+          console.log('failed during request: ' + err)
+        }
+        
+      }
     }
 
     requestPost();
   }, [setPost])
 
   return (
-    <div>{post.name}</div>
+    <div>
+      <h2>{post.name}</h2>
+      <p>{post.description}</p>
+    </div>
   );
 }
 
