1e5be7165615996dedf16afb8533664130ffb589
Jonathan Ellington <ellingtonjp@gmail.com>
2021-09-21 11:11:03 -0700

    remove integration test, cleanup comments
    Test for creating a new post gave warnings regarding wrapping test in
    act(). It seems related to the async/api functionality included in
    the test. For now, just commenting out.
    
    Next: try cypress for integration tests.

--- my_frontend/src/App.test.tsx
+++ my_frontend/src/App.test.tsx
@@ -4,22 +4,22 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("test", () => {
-  expect(true).toBeTruthy();
-});
+// test("test", () => {
+//   expect(true).toBeTruthy();
+// });
 
-test("happy path - I should be able to create a new Post", async () => {
-  render(<App />);
+// test("happy path - I should be able to create a new Post", async () => {
+//   render(<App />);
 
-  const nameInput = screen.getByLabelText("Name");
-  userEvent.type(nameInput, "bob");
+//   const nameInput = screen.getByLabelText("Name");
+//   userEvent.type(nameInput, "bob");
 
-  const descInput = screen.getByLabelText("Description");
-  userEvent.type(descInput, "Hi I am the description");
+//   const descInput = screen.getByLabelText("Description");
+//   userEvent.type(descInput, "Hi I am the description");
 
-  const button = screen.getByRole("button");
-  userEvent.click(button);
+//   const button = screen.getByRole("button");
+//   userEvent.click(button);
 
-  const successAlert = await screen.findByText("Success");
-  expect(successAlert).toBeVisible;
-});
+//   const successAlert = await screen.findByText("Success");
+//   expect(successAlert).toBeVisible;
+// });
